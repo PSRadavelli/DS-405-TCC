78a25f0ac7641fc32b71105281771df4978875ef
PAULO SERGIO RADAVELLI <u20797@LAPA30.academico.cotuca.unicamp.br>
2022-08-22 19:17:55 -0300

    start back-end module component

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: '3306',
+      username: 'root',
+      password: 'root',
+      database: '',
+      entities: [],
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule {
+  constructor(private dataSource: DataSource) {}
+}

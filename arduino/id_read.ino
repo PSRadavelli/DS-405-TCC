ace4e294c4316af1f5a9daf24f2118c25c215e76
Vinicius Granado <63064710+ViniciusGranado@users.noreply.github.com>
2022-11-05 17:49:08 -0300

    Update id_read.ino

--- arduino/id_read.ino
+++ arduino/id_read.ino
@@ -1,23 +1,43 @@
 #include <SPI.h>
 #include <MFRC522.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
 
 #define RST_PIN         22
 #define SS_PIN          5
 #define RELAY_PORT      27
 
+const char* ssid = "Vinicius 2.4GHz";
+const char* password = "vibrafone";
+
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 
 void setup() {
   Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+
+  Serial.print("Conectando ao WiFi");
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+
+  Serial.println("\nConectado a rede WiFi");
+  Serial.print("Endereço IP: ");
+  Serial.println(WiFi.localIP());
+
   SPI.begin();
 
   mfrc522.PCD_Init();
   mfrc522.PCD_DumpVersionToSerial();
 
-  Serial.println("Aproxime a tag...");
+  Serial.println("\nAproxime a tag...");
 
   pinMode(RELAY_PORT, OUTPUT);
-  digitalWrite(RELAY_PORT, LOW);
+  digitalWrite(RELAY_PORT, HIGH);
 }
 
 void loop() {
@@ -42,14 +62,67 @@ void loop() {
   Serial.println();
   conteudo.toUpperCase();
 
-  if (conteudo.substring(1) == "FB 91 47 53")
-  {
-    Serial.println("AUTORIZADO !");
-    Serial.println();
-    digitalWrite(RELAY_PORT, HIGH); // ativa rele, abre a trava solenoide
-    delay(3000);           // espera 3 segundos
-    digitalWrite(RELAY_PORT, LOW);  // desativa rele, fecha a trava solenoide
+  checaTag(conteudo);
+}
+
+void checaTag(String conteudoTag) {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient client;
+
+    String tagUID = conteudoTag.substring(1);
+
+    client.begin("https://tcc-cotuca-backend.viniciusgranado.repl.co/users/" + tagUID);
+
+    int httpCode = client.GET();
+
+    if (httpCode > 0) {
+      String payload = client.getString();
+      Serial.println("\nStatus code: " + String(httpCode));
+      Serial.println(payload);
+
+      char json[500];
+
+      payload.replace(" ", "");
+      payload.replace("\n", "");
+      payload.trim();
+      payload.toCharArray(json, 500);
+
+      Serial.println(payload);
+
+      StaticJsonDocument<200> doc;
+      DeserializationError err = deserializeJson(doc, json);
+
+      if (err) {
+        Serial.println("Error");
+      } else {
+        Serial.println("Success");
+      }
+
+      JsonObject obj = doc.as<JsonObject>();
+
+      if (obj.containsKey("id")) {
+        Serial.println("Contains");
+      } else {
+        Serial.println("Not contains");
+      }
+
+      int id = doc["id"];
+      int packageId = doc["packageId"];
+      const char* pkgSize = doc["size"];
+      int doorNumber = doc["doorNumber"];
+
+      Serial.println("--------------------------");
+      Serial.println(String(id));
+      Serial.println(String(packageId));
+      Serial.println(String(pkgSize));
+      Serial.println(String(doorNumber));
+      Serial.println("--------------------------");
+
+      client.end();
+    } else {
+      Serial.println("Erro na requisição HTTP");
+    }
   } else {
-    delay(3000);
+    Serial.println("Conexao perdida");
   }
 }
